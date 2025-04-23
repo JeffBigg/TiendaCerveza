1bf9d0696a7a96772c07c8f8edbbd700bdad3fe1
Deivis <soriaarandajeferson@gmail.com>
2025-04-23 18:09:05 -0500

    modificando footer

--- js/contacto.js
+++ js/contacto.js
@@ -16,13 +16,4 @@ document.getElementById('form-contacto').addEventListener('submit', function (e)
     document.getElementById('form-contacto').reset();
 
 
-    window.addEventListener('scroll', function () {
-        var footer = document.querySelector('.footer-mallki');
-        if (window.scrollY > 100) { // Puedes ajustar el valor 100 según cuándo quieras mostrar el footer
-            footer.style.display = 'block';
-        } else {
-            footer.style.display = 'none';
-        }
-    });
-
 });
--- js/tienda.js
+++
@@ -1,9 +0,0 @@
-// Puedes agregar funciones JS aquí si lo necesitas en el futuro.<script>
-window.addEventListener('scroll', function () {
-    var footer = document.querySelector('.footer-mallki');
-    if (window.scrollY > 100) { // Puedes ajustar el valor 100 según cuándo quieras mostrar el footer
-        footer.style.display = 'block';
-    } else {
-        footer.style.display = 'none';
-    }
-});
